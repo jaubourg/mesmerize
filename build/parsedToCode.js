eeac07f110692c5f7996e0b33147674d11f9aa84
jaubourg <j@ubourg.net>
2016-03-21 01:43:08 +0100

    Be even smarter in handling duplicate parts

--- build/parsedToCode.js
+++ build/parsedToCode.js
@@ -7,10 +7,11 @@ function dataEncode( key ) {
 function deepEqual( array1, array2 ) {
 	const type1 = typeof array1;
 	const type2 = typeof array2;
-	if ( type1 !== type2 ) {
+	const isArray = Array.isArray( array1 );
+	if ( type1 !== type2 || isArray !== Array.isArray( array2 ) ) {
 		return false;
 	}
-	if ( type1 !== "object" ) {
+	if ( !isArray ) {
 		return array1 === array2;
 	}
 	return array1.isMap === array2.isMap && array1.length === array2.length && array1.find( function( value, index ) {
@@ -22,43 +23,63 @@ function encode( data, tab ) {
 	if ( typeof data === "string" ) {
 		return data;
 	}
-	tab = ( tab || "" ) + "\t";
-	if ( data.isMap ) {
-		return `new Map( [\n${
-			data.map( function( item ) {
-				return tab + encode( item, tab );
-			} ).join( ",\n" )
-		}\n] )`;
+	if ( Array.isArray( data ) ) {
+		tab = tab || "";
+		const nextTab = tab + "\t";
+		if ( data.isMap ) {
+			return `new Map( [\n${
+				data.map( function( item ) {
+					return nextTab + encode( item, nextTab );
+				} ).join( ",\n" )
+			}\n${tab}] )`;
+		}
+		return `[ ${data.map( function( item ) {
+			return encode( item, tab );
+		} ).join( ", " )} ]`;
 	}
-	return `[ ${data.map( encode ).join( ", " )} ]`;
+	return data.id ? `_${data.id}` : encode( data.value, tab );
 }
 
 module.exports = function( array ) {
 	const existing = [];
 	const findExistingSet = new Map();
 	const last = ( function toString( array ) {
-		if ( !Array.isArray( array ) ) {
+		const isArray = Array.isArray( array );
+		if ( isArray ) {
+			const isMap = Array.isArray( array[ 0 ] );
+			array = array.map( toString );
+			array.isMap = isMap;
+		} else if ( typeof array !== "string" ) {
 			return dataEncode( array );
+		} else {
+			array = JSON.stringify( array );
 		}
-		const isMap = Array.isArray( array[ 0 ] );
-		array = array.map( toString );
-		array.isMap = isMap;
 		let existingSet = findExistingSet.get( array.length );
 		if ( !existingSet ) {
 			findExistingSet.set( array.length, ( existingSet = new Set() ) );
 		}
-		for ( let id of existingSet ) {
-			if ( deepEqual( array, existing[ id - 1 ] ) ) {
-				return `_${id}`;
+		for ( let item of existingSet ) {
+			if ( deepEqual( array, item.value ) ) {
+				item.multiple = true;
+				return item;
 			}
 		}
-		existing.push( array );
-		existingSet.add( existing.length );
-		return `_${existing.length}`;
+		const data = {
+			value: array
+		};
+		existing.push( data );
+		existingSet.add( data );
+		return data;
 	} )( array );
+	let id = 0;
 	return `"use strict";\n\n` +
-		existing.map( function( data, index ) {
-			return `const _${index + 1} = ${encode( data )};`;
+		existing.filter( function( item ) {
+			if ( item.multiple ) {
+				item.id = ++id;
+				return true;
+			}
+		} ).map( function( data, index ) {
+			return `const _${index + 1} = ${encode( data.value )};`;
 		} ).join( "\n" ) +
-		`\nmodule.exports = ${last};\n`;
+		`\n\nmodule.exports = ${encode( last.value )};\n`;
 };
