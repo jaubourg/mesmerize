7ce9b6913726e35dea6e103dff86a64aa5cff37a
jaubourg <j@ubourg.net>
2016-03-30 16:31:01 +0200

    Be more consistent in naming files in preparation for a more generic approach

--- build/code/function/index.js
+++ build/code/function/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const parseToFuncs = require( "./parseToFuncs" );
-const funcsToCode = require( "./funcsToCode" );
+const parseToTree = require( "./parseToTree" );
+const treeToCode = require( "./treeToCode" );
 
 const rCode = /const\s+root\s+=[^\n]+/;
 const template = require( "fs" ).readFileSync( `${__dirname}/template.js`, "utf8" );
 
 module.exports = function( items ) {
-	return template.replace( rCode, funcsToCode( parseToFuncs( items ) ) );
+	return template.replace( rCode, treeToCode( parseToTree( items ) ) );
 };
